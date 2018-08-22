6c06944c1dc4c81971f37541c163b5fcb10d3444
Raphael von der Grün <raphinesse@gmail.com>
2018-08-22 19:55:15 +0200

    Test failure conditions

--- test.js
+++ test.js
@@ -5,15 +5,25 @@ import rewire from 'rewire'
 const TEST_FD = 42
 const TEST_RESULT = 'Callback result'
 const TEST_ARGS = Object.freeze(['testPath', 'r'])
+class TestError extends Error {}
 
-test('calls callback with temporary file descriptor', async t => {
+test.beforeEach(t => {
   const m = rewire('.')
+  const fs = {
+    openSync: sinon.stub().returns(TEST_FD),
+    closeSync: sinon.spy(),
+  }
   const fsP = {
     open: sinon.stub().resolves(TEST_FD),
     close: sinon.stub().resolves(),
   }
-  m.__set__({ fsP })
-  const callback = sinon.stub().resolves(TEST_RESULT)
+  m.__set__({ fs, fsP })
+  t.context = { m, fs, fsP, callback: sinon.stub() }
+})
+
+test('calls callback with temporary file descriptor', async t => {
+  const { m, fsP, callback } = t.context
+  callback.resolves(TEST_RESULT)
 
   const result = await m(...TEST_ARGS, callback)
   t.true(fsP.open.calledOnceWithExactly(...TEST_ARGS))
@@ -23,14 +33,25 @@ test('calls callback with temporary file descriptor', async t => {
   t.is(result, TEST_RESULT)
 })
 
+test('closes the file if callback rejects', async t => {
+  const { m, fsP, callback } = t.context
+  callback.rejects(new TestError())
+
+  await t.throws(m(...TEST_ARGS, callback), TestError)
+  t.true(fsP.close.calledOnceWithExactly(TEST_FD))
+})
+
+test('closes the file if callback throws', async t => {
+  const { m, fsP, callback } = t.context
+  callback.throws(new TestError())
+
+  await t.throws(m(...TEST_ARGS, callback), TestError)
+  t.true(fsP.close.calledOnceWithExactly(TEST_FD))
+})
+
 test('synchronously calls callback with temporary file descriptor', t => {
-  const m = rewire('.')
-  const fs = {
-    openSync: sinon.stub().returns(TEST_FD),
-    closeSync: sinon.spy(),
-  }
-  m.__set__({ fs })
-  const callback = sinon.stub().returns(TEST_RESULT)
+  const { m, fs, callback } = t.context
+  callback.returns(TEST_RESULT)
 
   const result = m.sync(...TEST_ARGS, callback)
   t.true(fs.openSync.calledOnceWithExactly(...TEST_ARGS))
@@ -38,4 +59,12 @@ test('synchronously calls callback with temporary file descriptor', t => {
   t.true(fs.closeSync.calledOnceWithExactly(TEST_FD))
   t.true(fs.openSync.calledBefore(fs.closeSync))
   t.is(result, TEST_RESULT)
+})
+
+test('synchronously closes the file if callback throws', t => {
+  const { m, fs, callback } = t.context
+  callback.throws(new TestError())
+
+  t.throws(_ => m.sync(...TEST_ARGS, callback), TestError)
+  t.true(fs.closeSync.calledOnceWithExactly(TEST_FD))
 })
